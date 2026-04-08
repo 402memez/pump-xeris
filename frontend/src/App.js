879bef226219686523f35bc0e231ad9978b27402
402memez <402memez@gmail.com>
2026-04-08 01:31:39 -0500

    Switched to HashRouter for mobile wallet browser compatibility

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,16 @@
 import "@/App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import RocketGamePage from "./pages/RocketGamePage";
 import { Toaster } from "./components/ui/sonner";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<RocketGamePage />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
       <Toaster />
     </div>
   );
