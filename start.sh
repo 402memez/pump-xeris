2a470ce43748995983b403d4e1848578d91cfd40
402memez <402memez@gmail.com>
2026-04-09 01:43:00 -0500

    Fix port binding and permissions

--- start.sh
+++ start.sh
@@ -2,5 +2,6 @@
 echo "🚀 Booting Xeris Oracle..."
 node backend/oracle.js &
 
-echo "🚀 Booting Python Game Server..."
-python3 backend/server.py
+echo "🚀 Booting Python Game Server on Port $PORT..."
+# We force the python server to use the port Railway gives us
+python3 backend/server.py --port $PORT
